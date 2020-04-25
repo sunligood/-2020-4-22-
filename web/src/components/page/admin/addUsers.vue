06fd9fc1246b20b97ad768d78287c88769b955cc
lucky-y <heyuan@huniutech.com>
2020-04-25 12:17:48 +0800

    emmm

--- web/src/components/page/admin/addUsers.vue
+++ web/src/components/page/admin/addUsers.vue
@@ -69,27 +69,32 @@ export default {
         address: ''
       },
       rules: {
+        studentNo: [
+          { required: true, message: '请输入学号', trigger: 'blur' }
+        ],
         name: [
-          { required: true, message: '请输入活动名称', trigger: 'blur' },
-          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+          { required: true, message: '请输入姓名', trigger: 'blur' }
         ],
-        region: [
-          { required: true, message: '请选择活动区域', trigger: 'change' }
+        sex: [
+          { required: true, message: '请输入姓名', trigger: 'blur' }
         ],
-        date1: [
-          { type: 'date', required: true, message: '请选择日期', trigger: 'change' }
+        tie: [
+          { required: true, message: '请选择系别', trigger: 'change' }
         ],
-        date2: [
-          { type: 'date', required: true, message: '请选择时间', trigger: 'change' }
+        major: [
+          { type: 'date', required: true, message: '请选择专业', trigger: 'change' }
         ],
-        type: [
-          { type: 'array', required: true, message: '请至少选择一个活动性质', trigger: 'change' }
+        class: [
+          { type: 'date', required: true, message: '请选择班级', trigger: 'change' }
         ],
-        resource: [
-          { required: true, message: '请选择活动资源', trigger: 'change' }
+        mobile: [
+          { required: true, message: '请输入电话', trigger: 'blur' }
         ],
-        desc: [
-          { required: true, message: '请填写活动形式', trigger: 'blur' }
+        email: [
+          { required: true, message: '请输入email', trigger: 'blur' }
+        ],
+        address: [
+          { required: true, message: '请输入地址', trigger: 'blur' }
         ]
       }
     }
